204d3bf51e170e88ba5ee7113a18ae4856bb9616
Jack <jackowendickson@gmail.com>
2022-03-02 12:09:56 +0000

    taking lunch... testing add drink to menu doesn't work....

--- src/pub.py
+++ src/pub.py
@@ -2,7 +2,13 @@ class Pub:
     def __init__(self, name, till):
         self.name = name
         self.till = till
-        # self.drink = drinks
+        self.drinks = []
 
     def increase_till(self, amount):
         self.till += amount
+
+    def add_drink_to_menu(self, drink):
+        self.drinks.append(drink)
+    
+    def check_number_of_drinks(self):
+        return len(self.drinks)
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -5,6 +5,8 @@ from src.drink import Drink
 class TestPub(unittest.TestCase):
     def setUp(self):
         self.pub = Pub("The Prancing Pony", 100.00)
+        self.drink1 = Drink("Tennants", 3.50)
+        self.drink2 = Drink("Merlot", 4.50)
 
     def test_pub_has_name(self):
         self.assertEqual("The Prancing Pony", self.pub.name)
@@ -18,3 +20,12 @@ class TestPub(unittest.TestCase):
         actual = self.pub.till
         self.assertEqual(expected, actual)
         # self.assertEqual(102.50, self.pub.till)
+    
+    def test_drinks_list_is_empty(self):
+        self.assertEqual([], self.pub.drinks)
+#
+    def test_add_drink_to_menu(self):
+        Pub.drinks.add_drink_to_menu(self.drink1)
+        number_of_drinks = Pub.check_number_of_drinks()
+
+        self.assertEqual(1, number_of_drinks)
