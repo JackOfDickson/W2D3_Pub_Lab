85d32649e44b8d3f1dc20ed1538b4fe9f93647d4
Jack <jackowendickson@gmail.com>
2022-03-02 11:30:43 +0000

    Initial  Commit

---
+++ tests/pub_test.py
@@ -0,0 +1,19 @@
+import unittest
+from src.pub import Pub
+
+class TestPub(unittest.TestCase):
+    def setUp(self):
+        self.pub = Pub("The Prancing Pony", 100.00)
+
+    def test_pub_has_name(self):
+        self.assertEqual("The Prancing Pony", self.pub.name)
+    
+    def test_pub_has_money(self):
+        self.assertEqual(100.00, self.pub.till)
+    
+    def test_increase_till(self):
+        self.pub.increase_till(2.50)
+        expected = 102.50
+        actual = self.pub.till
+        self.assertEqual(expected, actual)
+        # self.assertEqual(102.50, self.pub.till)
